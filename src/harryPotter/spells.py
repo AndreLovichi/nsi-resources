daff5b7b0fa4d7534b1a699cd234a114613f9251
Romain André-Lovichi <romain.andrelovichi@gmail.com>
2020-12-15 22:28:02 +0100

    Add incantation length for spells

--- src/harryPotter/spells.py
+++ src/harryPotter/spells.py
@@ -2,7 +2,7 @@ from utils import csvUtils
 
 SOURCE_CSV_FILEPATH = "src/harryPotter/spells.csv"
 DIST_CSV_FILEPATH = "dist/harryPotter/spells.csv"
-DIST_CSV_HEADERS = ["name", "effect", "type", "incantation", "light"]
+DIST_CSV_HEADERS = ["name", "effect", "type", "incantation", "light", "incantationLength"]
 
 
 class Spell:
@@ -12,10 +12,11 @@ class Spell:
         self.type = rawSpell["Type"]
         self.effect = rawSpell["Effect"]
         self.light = rawSpell["Light"]
+        self.incantationLength = len(rawSpell["Incantation"])
         
     def __repr__(self):
-        return "{0}\n - Effect: {1}\n - Type: {2}\n - Incantation: {3}\n - Light: {4}".format(
-            self.name.upper(), self.effect, self.type, self.incantation, self.light)
+        return "{0}\n - Effect: {1}\n - Type: {2}\n - Incantation: {3} ({5})\n - Light: {4}".format(
+            self.name.upper(), self.effect, self.type, self.incantation, self.light, self.incantationLength)
 
 
 def fetchAllSpells():
