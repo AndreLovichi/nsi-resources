2a5a4f5a7535a8554354c1a370e208cf624c5f7b
Romain André-Lovichi <romain.andrelovichi@gmail.com>
2020-12-16 21:02:56 +0100

    Update Harry Potter character table

--- src/harryPotter/characters.py
+++ src/harryPotter/characters.py
@@ -2,24 +2,23 @@ from utils import csvUtils
 
 SOURCE_CSV_FILEPATH = "src/harryPotter/characters.csv"
 DIST_CSV_FILEPATH = "dist/harryPotter/characters.csv"
-DIST_CSV_HEADERS = ["name", "house", "wand", "patronus", "gender", "species", "bloodStatus", "hairColor", "eyeColor"]
+DIST_CSV_HEADERS = ["firstName", "lastName", "house", "gender", "bloodStatus", "hairColor", "eyeColor"]
 
 
 class Character:
     def __init__(self, rawCharacter):
-        self.name = rawCharacter["Name"]
+        nameParts = rawCharacter["Name"].split()
+        self.firstName = " ".join(nameParts[:-1])
+        self.lastName = nameParts[-1]
         self.gender = rawCharacter["Gender"]
         self.house = rawCharacter["House"]
-        self.wand = rawCharacter["Wand"]
-        self.patronus = rawCharacter["Patronus"]
-        self.species = rawCharacter["Species"]
         self.bloodStatus = rawCharacter["Blood status"]
         self.hairColor = rawCharacter["Hair colour"]        
-        self.eyeColor = rawCharacter["Hair colour"]
+        self.eyeColor = rawCharacter["Eye colour"]
         
     def __repr__(self):
-        return "{0}\n - Gender: {1}\n - House: {2}\n - Wand: {3}\n - Patronus: {4}\n - Species: {5}\n - Blood status: {6}\n - Hair color: {7}\n - Eye color: {8}".format(
-            self.name.upper(), self.gender, self.house, self.wand, self.patronus, self.species, self.bloodStatus, self.hairColor, self.eyeColor)
+        return "{0} {1}\n - Gender: {2}\n - House: {3}\n - Blood status: {4}\n - Hair color: {5}\n - Eye color: {6}".format(
+            self.firstName.upper(), self.lastName.upper(), self.gender, self.house, self.bloodStatus, self.hairColor, self.eyeColor)
 
 
 def fetchAllCharacters():
@@ -29,6 +28,6 @@ def fetchAllCharacters():
 
 def createCharacterCSV():
     allCharacters = fetchAllCharacters()
-    characterRows = [character.__dict__ for character in allCharacters]
+    characterRows = [character.__dict__ for character in allCharacters if character.firstName != ""]
 
     csvUtils.saveAsCsv(DIST_CSV_FILEPATH, DIST_CSV_HEADERS, characterRows)
