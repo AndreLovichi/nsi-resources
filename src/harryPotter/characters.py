dc74c9bb9ded08b5885227154cb72fdcc50eb017
Romain André-Lovichi <romain.andrelovichi@gmail.com>
2020-12-16 22:00:41 +0100

    Update Harry Potter character table

--- src/harryPotter/characters.py
+++ src/harryPotter/characters.py
@@ -2,13 +2,14 @@ from utils import csvUtils
 
 SOURCE_CSV_FILEPATH = "src/harryPotter/characters.csv"
 DIST_CSV_FILEPATH = "dist/harryPotter/characters.csv"
-DIST_CSV_HEADERS = ["firstName", "lastName", "house", "gender", "bloodStatus", "hairColor", "eyeColor"]
+DIST_CSV_HEADERS = ["firstName", "middleNames", "lastName", "house", "gender", "bloodStatus", "hairColor", "eyeColor"]
 
 
 class Character:
     def __init__(self, rawCharacter):
         nameParts = rawCharacter["Name"].split()
-        self.firstName = " ".join(nameParts[:-1])
+        self.firstName = nameParts[0]
+        self.middleNames = " ".join(nameParts[1:-1])
         self.lastName = nameParts[-1]
         self.gender = rawCharacter["Gender"]
         self.house = rawCharacter["House"]
@@ -17,8 +18,8 @@ class Character:
         self.eyeColor = rawCharacter["Eye colour"]
         
     def __repr__(self):
-        return "{0} {1}\n - Gender: {2}\n - House: {3}\n - Blood status: {4}\n - Hair color: {5}\n - Eye color: {6}".format(
-            self.firstName.upper(), self.lastName.upper(), self.gender, self.house, self.bloodStatus, self.hairColor, self.eyeColor)
+        return "{0} {1} {2}\n - Gender: {3}\n - House: {4}\n - Blood status: {5}\n - Hair color: {6}\n - Eye color: {7}".format(
+            self.firstName.upper(), self.middleNames.upper(), self.lastName.upper(), self.gender, self.house, self.bloodStatus, self.hairColor, self.eyeColor)
 
 
 def fetchAllCharacters():
