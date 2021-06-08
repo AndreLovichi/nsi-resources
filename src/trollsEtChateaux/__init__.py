e41994afa8b7053d56994cd789cb117117f8147f
Romain André-Lovichi <romain.andrelovichi@gmail.com>
2021-06-08 21:52:57 +0200

    Remove Trolls & Castles (extracted to a dedicated repo)

--- src/trollsEtChateaux/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# -*-coding:utf-8 -*
-
-from trollsEtChateaux.entities import Classement, Partie, Strategie
-from trollsEtChateaux.core import jouerPartie, jouerPlusieursParties
-from trollsEtChateaux.strategies import strategieExemple1, strategieExemple2, strategieExemple3
